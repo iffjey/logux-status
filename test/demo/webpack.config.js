6e23fb659ca6aa07bfb15eb097b3007c29c695d7
Andrey Sitnik <andrey@sitnik.ru>
2017-03-15 19:07:55 +0300

    Inline JS into demo page

--- test/demo/webpack.config.js
+++ test/demo/webpack.config.js
@@ -1,3 +1,4 @@
+var ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var path = require('path')
 
@@ -17,7 +18,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'index.html'),
       filename: 'index.html'
-    })
+    }),
+    new ScriptExtHtmlWebpackPlugin({ inline: 'bundle' })
   ],
   devServer: {
     contentBase: path.join(__dirname, 'build')
