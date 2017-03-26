47cb503a6f96df1d7230af18768dd7ac48bb8beb
Andrey Sitnik <andrey@sitnik.ru>
2017-03-26 16:34:24 +0100

    Use Client events instead of Sync events for cross-tab synchronization

--- index.js
+++ index.js
@@ -9,11 +9,3 @@ module.exports = {
   log: log,
   favicon: favicon
 }
-
-/**
- * Any object with Sync instance in `sync` property.
- * For example, `logux-client` instance.
- *
- * @typedef {object} Syncable
- * @property {BaseSync} sync The sync instance.
- */
